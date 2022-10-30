e5b56872876b0d644bef9f3cdbd8cb6a824d8bcb
Sergey <Sergey40248@mail.ru>
2022-10-30 18:39:17 +0300

    programs

---
+++ task/Program.cs
@@ -0,0 +1,10 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string[] newArr = { "gggd", " ", "7", "hhh", "hfhfhfh" };
+        var resultArr = newArr.Where(x => x.Length < 4).ToArray();
+        for (int i = 0; i < resultArr.Length; i++)
+            Console.Write(@"""{1},"" ", i, resultArr[i]);
+    }
+}
